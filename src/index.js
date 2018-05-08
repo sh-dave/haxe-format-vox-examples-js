205f0743bb845ec8fbf8b6ce2b00c26c74275129
David Klein <dave@smokinheads.de>
2018-05-08 21:06:54 +0200

    showcase some node evaluation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,62 @@
-const VoxReader = require('@sh-dave/format-vox').VoxReader;
+const formatVox = require('@sh-dave/format-vox');
+const Reader = formatVox.VoxReader;
+const Nodes = formatVox.VoxNodeTools;
+const Tools = formatVox.VoxTools;
+
 const fs = require('fs');
 
-fs.readFile('assets/ephtracy.vox', (err, data) => {
+let indent = 0;
+
+const ind = ( o = 0 ) => Array(indent + o).join(' ');
+
+const walker = {
+	beginGraph: vox => {
+	},
+	
+	endGraph: vox => {
+	},
+
+	beginGroup: attributes => {		
+		const t = Tools.getTranslationFromDict(attributes, '_t');
+		const r = Tools.getRotationFromDict(attributes, '_r');
+		console.log(`${ind()}group (translation: ${t.x}/${t.y}/${t.z}; rotation: ${r._00}/${r._10}/${r._20}/${r._01}/${r._11}/${r._21}/${r._02}/${r._12}/${r._22};)`);
+		indent += 2;
+	},
+
+	endGroup: () => {
+		indent -= 2;
+		console.log(`${ind()}/group`);
+	},
+
+	onTransform: attributes => {
+		const t = Tools.getTranslationFromDict(attributes, '_t');
+		const r = Tools.getRotationFromDict(attributes, '_r');
+		console.log(`${ind()}transform (translation: ${t.x}/${t.y}/${t.z}; rotation: ${r._00}/${r._10}/${r._20}/${r._01}/${r._11}/${r._21}/${r._02}/${r._12}/${r._22};)`);
+		indent += 2;
+	},
+
+	onShape: (attributes, models) => {
+		const t = Tools.getTranslationFromDict(attributes, '_t');
+		const r = Tools.getRotationFromDict(attributes, '_r');
+		console.log(`${ind()}shape {\n${ind(2)}translation: ${t.x}/${t.y}/${t.z};\n${ind(2)}rotation: ${r._00}/${r._10}/${r._20}/${r._01}/${r._11}/${r._21}/${r._02}/${r._12}/${r._22};`);
+		let modelIds = models.map(m => m.modelId);
+		console.log(`${ind(2)}models: [${modelIds.join(',')}];`);
+		console.log(`${ind()}}`);
+		indent -= 2;
+	}
+}
+
+fs.readFile('assets/hfv-logo.vox', (err, data) => {
 	if (err) throw err;
 
-	var vox = VoxReader.read(data, (vox, err) => {
+	var vox = Reader.read(data, (vox, err) => {
 		if (err) throw err;
 	
-		console.log(vox);
+		// console.log(vox.sizes);
+		// console.log(vox.models);
+		// console.log(vox.materials);
+		// console.log(vox.nodeGraph);
 
-		// use vox.models to get the meshes
-		// use vox.nodeGraph to access to world builder nodes
-	});
-	
+		Nodes.walkNodeGraph(vox, walker);
+	});	
 });
